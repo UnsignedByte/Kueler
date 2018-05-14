187390f0cdfdd0c2007e8610707ca00b900c55ac
Edmund Lam <edlam1889@gmail.com>
2018-05-14 08:21:50 -0700

    add untranslate

--- dictionary/compileipa.py
+++ dictionary/compileipa.py
@@ -3,9 +3,17 @@ import re
 import pickle
 
 eipa = {}
+ipae = {}
 
 with open("engipa.txt", 'r') as f:
     eipa = dict((k, v.split(', ')) for (k, v) in list(itertools.zip_longest(*[iter([a.strip() for a in f.readlines()])] * 2, fillvalue="")))
     eipa = sorted(dict((v,k) for k in eipa for v in eipa[k]).items(), key=lambda x:len(re.search(r'\(\?:(.*)\)', x[0]).group(1)), reverse=True)
+
 with open('compiledipa.txt', 'wb') as f:
     pickle.dump(eipa, f)
+
+with open("ipaeng.txt", 'r') as f:
+    ipae = sorted(dict((k, re.search(r'<(.*?)>', v).group(1)) for (k, v) in list(itertools.zip_longest(*[iter([a.strip() for a in f.readlines()])] * 2, fillvalue=""))).items(), key=lambda x:len(x[0]), reverse=True)
+
+with open('compiledeng.txt', 'wb') as f:
+    pickle.dump(ipae, f)
--- translate.py
+++ translate.py
@@ -4,15 +4,21 @@ import itertools
 
 cmu = {}
 cmurev = {}
-normaltrans = {}
+normaltrans = []
+revtrans = []
+untransre = None
 
 with open("dictionary/compileddict.txt", 'rb') as f:
     cmu = pickle.load(f)
 with open("dictionary/compileddictrev.txt", 'rb') as f:
-    cmu = pickle.load(f)
+    cmurev = pickle.load(f)
 
 with open("dictionary/compiledipa.txt", 'rb') as f:
     normaltrans = pickle.load(f)
+with open("dictionary/compiledeng.txt", 'rb') as f:
+    revtrans = pickle.load(f)
+    untransre = re.compile('|'.join(k for (k,v) in revtrans))
+    revtrans = dict(revtrans)
 
 with open("dictionary/dialect.txt", 'r') as f:
     dialect = dict((k, re.findall('/(.*?)/', v)) for (k, v) in list(itertools.zip_longest(*[iter([a.strip() for a in f.readlines()])] * 2, fillvalue="")))
@@ -39,7 +45,7 @@ def transword(word):
 def translate(sentence):
     transsent = re.sub(r'[\w\']+', lambda x:transword(x.group(0)), sentence)
     return (transsent,
-            transsent,
+            untranslate(transsent),
             ' '.join(''.join(dialectrev[c]+mods[dialect[dialectrev[c]].index(c)] if c in dialectrev else c for c in x) for x in transsent.lower().split()),
             ' '.join(''.join((dialectrev2[c].lower() if dialect[dialectrev[c]].index(c)==1 else dialectrev2[c]) if c in dialectrev2 else c for c in x) for x in transsent.lower().split())
             )
@@ -48,15 +54,12 @@ def untransword(word):
     try:
         ud = cmurev[word]
     except KeyError:
-        for a in normaltrans:
-            try:
-                ud = re.sub(a[0], a[1]+r'\1', ud)
-            except Exception:
-                ud = re.sub(a[0], a[1], ud)
+        ud = untransre.sub(lambda x:revtrans[x.group(0)], word).upper()
     return ud
 
 def untranslate(sentence):
-    transsent = re.sub(r'[^\pP\s-]+', lambda x:untransword(x.group(0)), sentence)
+    transsent = re.sub(r'[^\.,\?!\'\":;\-–—\s-]+', lambda x:untransword(x.group(0)), sentence)
+    return transsent
 
 while True:
     print('\n\n'.join(translate(input())))

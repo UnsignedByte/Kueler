3d722d6eccf06991c7afd7990644e3a17e5bbd18
Edmund Lam <edlam1889@gmail.com>
2018-05-14 18:04:57 -0700

    full trans capabilities

--- translate.py
+++ translate.py
@@ -1,7 +1,9 @@
 import pickle
 import re
 import itertools
+import sys
 
+thismodule = sys.modules[__name__]
 cmu = {}
 cmurev = {}
 normaltrans = []
@@ -20,46 +22,53 @@ with open("dictionary/compiledeng.txt", 'rb') as f:
     untransre = re.compile('|'.join(k for (k,v) in revtrans))
     revtrans = dict(revtrans)
 
-with open("dictionary/dialect.txt", 'r') as f:
-    dialect = dict((k, re.findall('/(.*?)/', v)) for (k, v) in list(itertools.zip_longest(*[iter([a.strip() for a in f.readlines()])] * 2, fillvalue="")))
-    dialectrev = dict((v,k) for k in dialect for v in dialect[k])
-
-with open("dictionary/dialect2.txt", 'r') as f:
-    dialect2 = dict((k, re.findall('/(.*?)/', v)) for (k, v) in list(itertools.zip_longest(*[iter([a.strip() for a in f.readlines()])] * 2, fillvalue="")))
-    dialectrev2 = dict((v,k) for k in dialect2 for v in dialect2[k])
+with open("dictionary/kueler.txt", 'r') as f:
+    kueler = dict((k, re.findall('/(.*?)/', v)) for (k, v) in list(itertools.zip_longest(*[iter([a.strip() for a in f.readlines()])] * 2, fillvalue="")))
+    kuelerrev = dict((v,k) for k in kueler for v in kueler[k])
 
 mods = ['', 'Y', 'O', 'OY']
 
-def transword(word):
-    try:
-        ud = cmu[word.upper()][0]
-    except KeyError:
-        ud = re.sub(r'(\w)\1+', r'\1\1', word).lower()
-        for a in normaltrans:
-            try:
-                ud = re.sub(a[0], a[1]+r'\1', ud)
-            except Exception:
-                ud = re.sub(a[0], a[1], ud)
-    return ud
+def translate(sentence, f, t):
+    return getattr(thismodule, f+t)(sentence)
 
-def translate(sentence):
-    transsent = re.sub(r'[\w\']+', lambda x:transword(x.group(0)), sentence)
-    return (transsent,
-            untranslate(transsent),
-            ' '.join(''.join(dialectrev[c]+mods[dialect[dialectrev[c]].index(c)] if c in dialectrev else c for c in x) for x in transsent.lower().split()),
-            ' '.join(''.join((dialectrev2[c].lower() if dialect[dialectrev[c]].index(c)==1 else dialectrev2[c]) if c in dialectrev2 else c for c in x) for x in transsent.lower().split())
-            )
-
-def untransword(word):
-    try:
-        ud = cmurev[word]
-    except KeyError:
-        ud = untransre.sub(lambda x:revtrans[x.group(0)], word).upper()
-    return ud
-
-def untranslate(sentence):
-    transsent = re.sub(r'[^\.,\?!\'\":;\-–—\s-]+', lambda x:untransword(x.group(0)), sentence)
+def engipa(sentence):
+    print("hello hello")
+    def engipaword(word):
+        try:
+            ud = cmu[word.upper()][0]
+        except KeyError:
+            ud = re.sub(r'(\w)\1+', r'\1\1', word).lower()
+            for a in normaltrans:
+                try:
+                    ud = re.sub(a[0], a[1]+r'\1', ud)
+                except Exception:
+                    ud = re.sub(a[0], a[1], ud)
+        return ud
+    transsent = re.sub(r'[\w\']+', lambda x:engipaword(x.group(0)), sentence)
     return transsent
 
+def ipaeng(sentence):
+    def ipawordeng(word):
+        try:
+            ud = cmurev[word]
+        except KeyError:
+            ud = untransre.sub(lambda x:revtrans[x.group(0)], word).upper()
+        return ud
+    transsent = re.sub(r'[\w]+', lambda x:ipawordeng(x.group(0)), sentence)
+    return transsent
+
+def kueleripa(sentence):
+    return re.sub(r'[a-zA-Z](?:h|H)?', lambda x:(lambda k:(kueler[k][0] if k.isupper() else kueler[k.upper()][1]))(x.group(0)), sentence)
+
+def ipakueler(sentence):
+    print(sentence)
+    return re.sub(r'[\w]', lambda x:(lambda c:kuelerrev[c].lower() if kueler[kuelerrev[c]].index(c)==1 else kuelerrev[c])(x.group(0)), sentence)
+
+def kuelereng(sentence):
+    return ipaeng(kueleripa(sentence))
+
+def engkueler(sentence):
+    return ipakueler(engipa(sentence))
+
 while True:
-    print('\n\n'.join(translate(input())))
+    print(translate(input(), 'eng', 'kueler'))
